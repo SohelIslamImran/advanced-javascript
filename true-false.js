a4187534eed1f444ec712e3de6412ad913e4f3b0
Sohel Islam Imran <sohelislamimran@gmail.com>
2021-02-02 16:45:20 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,10 @@
+// Falsy : false, 0, "", undefined, NaN, null.
+// Truthy : "0", " ", [], "false".
+
+let name = 12;
+if (name) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
