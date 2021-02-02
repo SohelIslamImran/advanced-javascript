b2bca4044bc6bcc56a93b7ae19c0422a3b884c4b
Sohel Islam Imran <sohelislamimran@gmail.com>
2021-02-02 18:57:13 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+let num;
+console.log(num);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(12, 18);
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+const result = add(18);
+
+const details = { name: 'Abdullah', phone: 017725498};
+console.log(details);
+
+let fun = undefined;
+console.log(fun);
+
+const age = [12, 14, 16, 18];
+console.log(age[12]);
+
+null;
